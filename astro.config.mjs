3e5b5afc0139353bf68fcd5097e4204649a1efdd
erensarac <erenkansarac@hotmail.com>
2025-03-11 07:09:27 +0300

    feat: configure tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://getting-started-astro.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
